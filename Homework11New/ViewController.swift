16b9b24c6a0e2f07d3509bd1d8bdc6c4b8edf663
Nikita Mashtakov <n-mashtakov@yandex.ru>
2022-03-31 13:38:38 +0700

    Initial Commit

---
+++ Homework11New/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework11New
+//
+//  Created by Никита Маштаков on 31.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
